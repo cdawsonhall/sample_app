9640d40b9f9cff6e4327538ed020e3a7fdfe1adf
Corey Hall <cdawsonhall@gmail.com>
2013-08-29 11:41:03 -0700

    pages routes added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,7 @@ describe PagesController do
   render_views
   
   describe "GET 'home'" do
-    it "returns http success" do
+    it "should be sucessful" do
       get 'home'
       response.should be_success
     end
@@ -16,17 +16,38 @@ describe PagesController do
   end
 
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "should be sucessful" do
       get 'contact'
       response.should be_success
     end
+
+    # it "should have the right title" do
+    #   get 'contact'
+    #   response.should have_selector("title", :content => "#{@base_title} | Contact")
+    # end
   end
 
   describe "GET 'about'" do
-    it "returns http success" do
+    it "should be sucessful" do
       get 'about'
       response.should be_success
     end
+
+    # it "should have the right title" do
+    #   get 'about'
+    #   response.should have_selector("title", :content => "#{@base_title} | About")
+    # end
   end
 
+  describe "GET 'help'" do
+    it "should be sucessful" do
+      get 'help'
+      response.should be_success
+    end
+
+    # it "should have the right title" do
+    #   get 'help'
+    #   response.should have_selector("title", :content => "#{@base_title} | Help")
+    # end
+  end
 end
