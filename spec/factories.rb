712a9c21038fea142e3c2b0ce18519b1ead8fdc2
Corey Hall <cdawsonhall@gmail.com>
2013-08-30 13:58:26 -0700

    finished signup

--- spec/factories.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user do |user|
-    user.name "Michael Hartl"
-    user.email "mhartl@example.com"
-    user.password "foobar"
-    user.password_confirmation "foobar"
-  end
-end 
