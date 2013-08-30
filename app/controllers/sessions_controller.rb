71ef56617bdb3d1c82c527bea24e5ad61af2995a
Corey Hall <cdawsonhall@gmail.com>
2013-08-30 15:17:18 -0700

    added signin view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.authenticate(params[:session][:email], params[:session][:password])
+
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination."
+      @title = "Sign In"
+      render 'new'
+    else
+      #handle successful signin.
+    end      
   end
 
   def destroy
     
   end
-end
+end 
